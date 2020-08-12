6246b2f21ca9160bb355e4c0d47b324a1a7815c9
patrick_peng <patrick_peng@mail.gss.com.tw>
2020-08-12 11:29:40 +0800

    TEST

--- Practice_Architecture/Architecture.Web/Controllers/HomeController.cs
+++ Practice_Architecture/Architecture.Web/Controllers/HomeController.cs
@@ -22,7 +22,7 @@ namespace Architecture.Web.Controllers
         #region
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+             ViewBag.Message = "Your application description page.";
 
             return View();
         }
